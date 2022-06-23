dc6794f80ee33154144f1a895e9f70ac1bddbafc
Manav <dixitmanav.987@gmail.com>
2022-06-23 10:46:27 +0530

    Font changed to poppins

--- frontend/src/components/PreviousButton/PreviousButton.js
+++ frontend/src/components/PreviousButton/PreviousButton.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faArrowLeftLong} from '@fortawesome/free-solid-svg-icons';
-import {useNavigate,useLocation} from 'react-router-dom'
+import {useNavigate}  from 'react-router-dom'
 import './PreviousButton.css';
 
 const PreviousButton = () => {
     const navigate=useNavigate();
-    const location=useLocation();
     const goBack=()=>{
         navigate(-1);
     }
